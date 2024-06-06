7fa858115fd58fb014140597684af11073d441aa
Greih Murray <murrayg0216@outlook.com>
2024-06-06 06:17:03 -0500

    Module 2

---
+++ module2/untitled/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Recursive sum with i=4: " + recurse(4d));
+        System.out.println("Recursive sum with i=9: " + recurse(9d));
+        System.out.println("Recursive sum with i=14: " + recurse(14d));
+    }
+
+    public static double recurse(double i){
+        if(i >= 1){
+            double temp = (i/(i+1d)) + (Double)recurse(i-1d);
+            return temp;
+        } else{
+            return (1d/2d);
+        }
+    }
+}
